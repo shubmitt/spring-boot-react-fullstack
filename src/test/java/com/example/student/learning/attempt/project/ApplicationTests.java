78d3d60a88c4ca8db783b16a06ad4839baef1c09
shubmitt <39769943+shubmitt@users.noreply.github.com>
2025-01-23 18:04:43 -0800

    Update ApplicationTests.java

--- src/test/java/com/example/student/learning/attempt/project/ApplicationTests.java
+++ src/test/java/com/example/student/learning/attempt/project/ApplicationTests.java
@@ -1,7 +1,7 @@
 package com.example.student.learning.attempt.project;
 
 //import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
